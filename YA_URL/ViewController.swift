5f32afddc07becc12a62b6e84a0e870416119ec8
macsonm <75022784+macsonm@users.noreply.github.com>
2022-02-09 16:51:16 +0300

    Initial Commit

---
+++ YA_URL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YA_URL
+//
+//  Created by Maxim M on 09.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
