3aba481cef318b217c3bc4bf2aa160971e8ce569
macsonm <75022784+macsonm@users.noreply.github.com>
2022-02-09 17:32:49 +0300

    URL
    get image

--- YA_URL/ViewController.swift
+++ YA_URL/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  YA_URL
-//
-//  Created by Maxim M on 09.02.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let linkPicture = "https://mota.ru/upload/wallpapers/source/2014/12/03/20/02/42102/005.jpg"
+        
+        guard let url = URL(string: linkPicture) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            //print("Done")
+            
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.imageView?.image = image
+            }
+        }
+        
+        task.resume()
+        
     }
 
-
 }
-
